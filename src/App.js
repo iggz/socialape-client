649762ffaeb4808e23bb63aab44b2f96f036a3a2
iggz <ipopenov@protonmail.com>
2019-12-22 02:17:25 -0500

    Created Signup and Auth state. About to implement redux

--- src/App.js
+++ src/App.js
@@ -3,34 +3,33 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
 
 // Components
 import NavBar from "./components/NavBar";
+import AuthRoute from "./util/AuthRoute";
 
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: "#33c9dc",
-			main: "#00bcd4",
-			dark: "#008394",
-			contrastText: "#fff"
-		},
-		secondary: {
-			light: "#ff6333",
-			main: "#ff3d00",
-			dark: "#b22a00",
-			contrastText: "#fff"
-		},
-		typography: {
-			useNextVariants: true
-		}
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+
+const token = localStorage.FBIdToken;
+if (token) {
+	const decodedToken = jwtDecode(token);
+	console.log(decodedToken);
+	if (decodedToken.exp * 1000 < Date.now()) {
+		window.location.href = "/login";
+		authenticated = false;
+	} else {
+		authenticated = true;
 	}
-});
+}
 
 export class App extends Component {
 	render() {
@@ -42,11 +41,17 @@ export class App extends Component {
 						<div className="container">
 							<Switch>
 								<Route exact path="/" component={home} />
-								<Route exact path="/login" component={login} />
-								<Route
+								<AuthRoute
+									exact
+									path="/login"
+									component={login}
+									authenticated={authenticated}
+								/>
+								<AuthRoute
 									exact
 									path="/signup"
 									component={signup}
+									authenticated={authenticated}
 								/>
 							</Switch>
 						</div>
