bed703375b900af5dea84fc3b9206e8e939a59e0
iggz <ipopenov@protonmail.com>
2019-12-22 20:52:55 -0500

    Profile section is up and running

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 
-import Scream from '../components/Scream';
+import Scream from "../components/Scream";
+import Profile from "../components/Profile";
 
 class home extends Component {
 	state = {
@@ -10,7 +11,7 @@ class home extends Component {
 	};
 
 	componentDidMount() {
-		axios.get("/screams").then(res => {
+		axios.get("/screams").then((res) => {
 			this.setState({
 				screams: res.data
 			});
@@ -19,17 +20,19 @@ class home extends Component {
 
 	render() {
 		let recentScreamsMarkup = this.state.screams ? (
-			this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+			this.state.screams.map((scream) => (
+				<Scream key={scream.screamId} scream={scream} />
+			))
 		) : (
-				<p>Loading...</p>
-			);
+			<p>Loading...</p>
+		);
 		return (
 			<Grid container spacing={10}>
 				<Grid item sm={8} xs={12}>
 					{recentScreamsMarkup}
 				</Grid>
 				<Grid item sm={4} xs={12}>
-					<p>Profile...</p>
+					<Profile />
 				</Grid>
 			</Grid>
 		);
